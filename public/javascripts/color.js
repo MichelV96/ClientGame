cba8558cc3ac113b183e5e089051719489987ecf
MichelV <meneervaartjes@hotmail.com>
2017-03-23 10:54:55 +0100

    Tekenen toegeveogd

---
+++ public/javascripts/color.js
@@ -0,0 +1,27 @@
+$('#purple').click(function () {
+    pencilColor = "purple";
+});
+
+$('#blue').click(function () {
+    pencilColor = "blue";
+});
+
+$('#red').click(function () {
+    pencilColor = "red";
+});
+
+$('#green').click(function () {
+    pencilColor = "green";
+});
+
+$('#orange').click(function () {
+    pencilColor = "orange";
+});
+
+$('#yellow').click(function () {
+    pencilColor = "yellow";
+});
+
+$('#black').click(function () {
+    pencilColor = "black";
+});
