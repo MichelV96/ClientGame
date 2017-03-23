d16d0690d104377320b3488a180edb45dc5eda03
LENOVO-PC-RUBEN\Gebruiker <ruben.spanje@gmail.com>
2017-03-23 13:49:16 +0100

    Gum

--- public/javascripts/color.js
+++ public/javascripts/color.js
@@ -1,27 +1,38 @@
 $('#purple').click(function () {
     pencilColor = "purple";
+    changePencil();
 });
 
 $('#blue').click(function () {
     pencilColor = "blue";
+    changePencil();
 });
 
 $('#red').click(function () {
     pencilColor = "red";
+    changePencil();
 });
 
 $('#green').click(function () {
     pencilColor = "green";
+    changePencil();
 });
 
 $('#orange').click(function () {
     pencilColor = "orange";
+    changePencil();
 });
 
 $('#yellow').click(function () {
     pencilColor = "yellow";
+    changePencil();
 });
 
 $('#black').click(function () {
     pencilColor = "black";
+    changePencil();
 });
+
+var changePencil = function() {
+    $('#canvas').css("cursor", "url('../images/pencil.png'), pointer")
+}
